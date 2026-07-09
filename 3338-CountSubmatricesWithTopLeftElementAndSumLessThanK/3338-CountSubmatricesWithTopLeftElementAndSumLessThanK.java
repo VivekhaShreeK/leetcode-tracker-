e4e338b617cda361d16808d590e2b970c90084a2
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-07-09 09:47:16 +0530

    Create 3338-CountSubmatricesWithTopLeftElementAndSumLessThanK

---
+++ 3338-CountSubmatricesWithTopLeftElementAndSumLessThanK/3338-CountSubmatricesWithTopLeftElementAndSumLessThanK.java
@@ -0,0 +1,19 @@
+// Last updated: 7/9/2026, 9:47:16 AM
+class Solution {
+    public int countSubmatrices(int[][] grid, int k) {
+        int n = grid.length, m = grid[0].length;
+        int[] cols = new int[m];
+        int res = 0;
+        for(int i = 0; i < n; i++) {
+            int rows = 0;
+            for(int j = 0; j < m; j++) {
+                cols[j] += grid[i][j];
+                rows += cols[j];
+                if(rows <= k) {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+}
