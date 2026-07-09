b294613ec13a61b0cdb2f4e04097834abd4a6b60
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-07-09 09:48:18 +0530

    Create 2300-ConstructStringWithRepeatLimit

---
+++ 2300-ConstructStringWithRepeatLimit/2300-ConstructStringWithRepeatLimit.java
@@ -0,0 +1,34 @@
+// Last updated: 7/9/2026, 9:48:18 AM
+class Solution {
+    public String repeatLimitedString(String s, int repeatLimit) {
+        int[] freq = new int[26];
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+        StringBuilder result = new StringBuilder();
+        int currentCharIndex = 25;
+        while (currentCharIndex >= 0) {
+            if (freq[currentCharIndex] == 0) {
+                currentCharIndex--;
+                continue;
+            }
+            int use = Math.min(freq[currentCharIndex], repeatLimit);
+            for (int k = 0; k < use; k++) {
+                result.append((char) ('a' + currentCharIndex));
+            }
+            freq[currentCharIndex] -= use;
+            if (freq[currentCharIndex] > 0) {
+                int smallerCharIndex = currentCharIndex - 1;
+                while (smallerCharIndex >= 0 && freq[smallerCharIndex] == 0) {
+                    smallerCharIndex--;
+                }
+                if (smallerCharIndex < 0) {
+                    break;
+                }
+                result.append((char) ('a' + smallerCharIndex));
+                freq[smallerCharIndex]--;
+            }
+        }
+        return result.toString();
+    }
+}
