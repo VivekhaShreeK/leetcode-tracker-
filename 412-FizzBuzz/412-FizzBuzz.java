f7ee3eab6329be004e3f3b623f195fa8edea476e
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-07-09 09:51:54 +0530

    Create 412-FizzBuzz

---
+++ 412-FizzBuzz/412-FizzBuzz.java
@@ -0,0 +1,16 @@
+// Last updated: 7/9/2026, 9:51:54 AM
+public class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> a = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                a.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                a.add("Fizz");
+            } else if (i % 5 == 0) {
+                a.add("Buzz");
+            } else {
+                a.add(String.valueOf(i)); }}
+        return a;
+    }
+}
