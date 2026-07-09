da7daa81b2f4dbeb09c9e2b012b49e464a0b00e7
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-07-09 09:48:30 +0530

    Create 2190-CountCommonWordsWithOneOccurrence

---
+++ 2190-CountCommonWordsWithOneOccurrence/2190-CountCommonWordsWithOneOccurrence.java
@@ -0,0 +1,20 @@
+// Last updated: 7/9/2026, 9:48:30 AM
+class Solution {
+    public int countWords(String[] words1, String[] words2) {
+        Map<String, Integer> map1 = new HashMap<>();
+        Map<String, Integer> map2 = new HashMap<>();
+        for (String w : words1) {
+            map1.put(w, map1.getOrDefault(w, 0) + 1);
+        }
+        for (String w : words2) {
+            map2.put(w, map2.getOrDefault(w, 0) + 1);
+        }
+        int count = 0;
+        for (String w : map1.keySet()) {
+            if (map1.get(w) == 1 && map2.getOrDefault(w, 0) == 1) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
