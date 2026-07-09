3923c4439a7a45683ff018448c3d3e1f1f266b3d
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-07-09 09:48:36 +0530

    Create 2117-FindOriginalArrayFromDoubledArray

---
+++ 2117-FindOriginalArrayFromDoubledArray/2117-FindOriginalArrayFromDoubledArray.java
@@ -0,0 +1,29 @@
+// Last updated: 7/9/2026, 9:48:36 AM
+class Solution {
+    public int[] findOriginalArray(int[] changed) {
+        int n = changed.length;
+        if (n % 2 != 0) return new int[0];
+        Arrays.sort(changed);
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int x : changed) {
+            freq.put(x, freq.getOrDefault(x, 0) + 1);
+        }
+        int[] ans = new int[n / 2];
+        int idx = 0;
+        for (int x : changed) {
+            if (freq.get(x) == 0) continue;
+            if (x == 0) {
+                if (freq.get(0) < 2) return new int[0];
+                ans[idx++] = 0;
+                freq.put(0, freq.get(0) - 2);
+            } else {
+                if (freq.getOrDefault(2 * x, 0) == 0)
+                    return new int[0];
+                ans[idx++] = x;
+                freq.put(x, freq.get(x) - 1);
+                freq.put(2 * x, freq.get(2 * x) - 1);
+            }
+        }
+        return idx == n / 2 ? ans : new int[0];
+    }
+}
