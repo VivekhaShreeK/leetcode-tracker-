228a7579a15eb599c14a5a2cca00fb15ae5ed30e
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-07-09 09:48:03 +0530

    Create 2547-OddStringDifference

---
+++ 2547-OddStringDifference/2547-OddStringDifference.java
@@ -0,0 +1,24 @@
+// Last updated: 7/9/2026, 9:48:03 AM
+class Solution {
+    public String oddString(String[] words) {
+        HashMap<String, Integer> map = new HashMap<>();
+        HashMap<String, String> patternMap = new HashMap<>();
+        for(int i = 0; i < words.length; i++){
+            StringBuilder sb = new StringBuilder();
+            String word = words[i];
+            for(int j = 0; j < word.length() - 1; j++){
+                int diff = word.charAt(j + 1) - word.charAt(j);
+                sb.append(diff).append(",");
+            }
+            String pattern = sb.toString();
+            map.put(pattern, map.getOrDefault(pattern, 0) + 1);
+            patternMap.put(pattern, word);
+        }
+        for(String pattern : map.keySet()){
+            if(map.get(pattern) == 1){
+                return patternMap.get(pattern);
+            }
+        }
+        return "";
+    }
+} 
