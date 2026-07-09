6e920a2debbe507b4a57531ff480d4f06b3e6f3b
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-07-09 09:49:37 +0530

    Create 1647-CanConvertStringInKMoves

---
+++ 1647-CanConvertStringInKMoves/1647-CanConvertStringInKMoves.java
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 9:49:37 AM
+class Solution {
+        public boolean canConvertString(String s, String t, int k) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); ++i) {
+            int diff = (t.charAt(i) - s.charAt(i) + 26) % 26;
+            if (diff > 0 && diff + count[diff] * 26 > k) {
+                return false;
+            }
+            ++count[diff];
+        }
+        return true;
+    }
+}
