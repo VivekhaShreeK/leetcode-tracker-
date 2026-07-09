91066eb3ead8fe16fd069cc54ccec85a567042e9
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-07-09 09:48:48 +0530

    Create 2001-JumpGameVii

---
+++ 2001-JumpGameVii/2001-JumpGameVii.java
@@ -0,0 +1,23 @@
+// Last updated: 7/9/2026, 9:48:48 AM
+class Solution {
+
+    public boolean canReach(String s, int minJump, int maxJump) {
+        int n = s.length();
+        int[] f = new int[n];
+        int[] pre = new int[n];
+        f[0] = 1;
+        for (int i = 0; i < minJump; i++) {
+            pre[i] = 1;
+        }
+        for (int i = minJump; i < n; i++) {
+            int left = i - maxJump;
+            int right = i - minJump;
+            if (s.charAt(i) == '0') {
+                int total = pre[right] - (left <= 0 ? 0 : pre[left - 1]);
+                f[i] = total != 0 ? 1 : 0;
+            }
+            pre[i] = pre[i - 1] + f[i];
+        }
+        return f[n - 1] == 1;
+    }
+}
