cd40f1e02d0a865537cf0c9b3adcf9c478832b01
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-07-09 09:45:59 +0530

    Create 4045-LongestBalancedSubarrayI

---
+++ 4045-LongestBalancedSubarrayI/4045-LongestBalancedSubarrayI.java
@@ -0,0 +1,23 @@
+// Last updated: 7/9/2026, 9:45:59 AM
+class Solution {
+    public int longestBalanced(int[] nums) {
+        int max = 0;
+        for(int i = 0; i < nums.length; i++) {
+            Set<Integer> even = new HashSet<>();
+            Set<Integer> odd = new HashSet<>();
+            for(int j = i; j < nums.length; j++) {
+                if(nums[j] % 2 == 0) {
+                    even.add(nums[j]);
+                }
+                else {
+                    odd.add(nums[j]);
+                }
+                if(even.size() == odd.size()) {
+                    max = Math.max(max, j-i+1);
+                }
+            }
+            
+        }
+        return max;
+    }
+}
