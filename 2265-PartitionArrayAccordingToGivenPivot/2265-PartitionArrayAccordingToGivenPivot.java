9188c5f670903d434ae1d3aa32c168d5348139e6
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-07-09 09:48:19 +0530

    Create 2265-PartitionArrayAccordingToGivenPivot

---
+++ 2265-PartitionArrayAccordingToGivenPivot/2265-PartitionArrayAccordingToGivenPivot.java
@@ -0,0 +1,25 @@
+// Last updated: 7/9/2026, 9:48:19 AM
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        LinkedList<Integer> less = new LinkedList<>();
+        LinkedList<Integer> equal = new LinkedList<>();
+        LinkedList<Integer> greater = new LinkedList<>();
+        for (int num : nums) {
+            if (num < pivot) {
+                less.add(num);
+            } else if (num > pivot) {
+                greater.add(num);
+            } else {
+                equal.add(num);
+            }
+        }
+        less.addAll(equal);
+        less.addAll(greater);
+        int i = 0;
+        int[] ans = new int[nums.length];
+        for (int num : less) {
+            ans[i++] = num;
+        }
+        return ans;
+    }
+}
