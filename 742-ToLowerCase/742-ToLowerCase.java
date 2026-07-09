ee90f0dfa61b418b7904d1ab13c98da8860fb2a3
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-07-09 09:51:08 +0530

    Create 742-ToLowerCase

---
+++ 742-ToLowerCase/742-ToLowerCase.java
@@ -0,0 +1,8 @@
+// Last updated: 7/9/2026, 9:51:08 AM
+class Solution {
+    public String toLowerCase(String s) {
+        Scanner x = new Scanner(System.in);
+        String s1 = s.toLowerCase();
+        return s1;
+    }
+}
