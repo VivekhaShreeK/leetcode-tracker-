37a2852acc16131f744b49045c41c4fc7c9cd117
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-07-09 09:51:40 +0530

    Create 495-TeemoAttacking

---
+++ 495-TeemoAttacking/495-TeemoAttacking.java
@@ -0,0 +1,12 @@
+// Last updated: 7/9/2026, 9:51:40 AM
+class Solution {
+  public int findPoisonedDuration(int[] timeSeries, int duration) {
+    int n = timeSeries.length;
+    if (n == 0) return 0;
+
+    int total = 0;
+    for(int i = 0; i < n - 1; ++i)
+      total += Math.min(timeSeries[i + 1] - timeSeries[i], duration);
+    return total + duration;
+  }
+}
