49ef2dc59993057e07f77f5f2783a08eadbe7e72
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-07-09 09:51:46 +0530

    Create 455-AssignCookies

---
+++ 455-AssignCookies/455-AssignCookies.java
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 9:51:46 AM
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int contentChildren = 0;
+        int cookieIndex = 0;
+        while (cookieIndex < s.length && contentChildren < g.length) {
+            if (s[cookieIndex] >= g[contentChildren]) {
+                contentChildren++;
+            }
+            cookieIndex++;
+        }
+        return contentChildren;
+    }
+
+}
