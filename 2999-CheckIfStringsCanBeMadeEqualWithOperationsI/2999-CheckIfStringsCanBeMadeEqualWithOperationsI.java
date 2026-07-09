945156bc313d1106a77a37801e9656cc900e7255
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-07-09 09:47:31 +0530

    Create 2999-CheckIfStringsCanBeMadeEqualWithOperationsI

---
+++ 2999-CheckIfStringsCanBeMadeEqualWithOperationsI/2999-CheckIfStringsCanBeMadeEqualWithOperationsI.java
@@ -0,0 +1,9 @@
+// Last updated: 7/9/2026, 9:47:31 AM
+class Solution {
+    public boolean canBeEqual(String s1, String s2) {
+        return ((s1.charAt(0) == s2.charAt(0) && s1.charAt(2) == s2.charAt(2)) ||
+                (s1.charAt(0) == s2.charAt(2) && s1.charAt(2) == s2.charAt(0))) &&
+               ((s1.charAt(1) == s2.charAt(1) && s1.charAt(3) == s2.charAt(3)) ||
+                (s1.charAt(1) == s2.charAt(3) && s1.charAt(3) == s2.charAt(1)));
+    }
+}
