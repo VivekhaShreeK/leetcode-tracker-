cf83894ebefb70a2d5fdc289aa13f2ddfc97634b
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-07-27 10:14:11 +0530

    Create 71-SimplifyPath

---
+++ 71-SimplifyPath/71-SimplifyPath.java
@@ -0,0 +1,25 @@
+// Last updated: 27/07/2026, 10:14:12
+class Solution {
+    public String simplifyPath(String path) {
+        String[] components = path.split("/");
+        Stack<String> stack = new Stack<>();
+        for(String comp : components) {
+            if(comp.equals("") || comp.equals(".")) {
+                continue;
+            }
+            if(comp.equals("..")) {
+                if(!stack.isEmpty()) {
+                    stack.pop();
+                }
+            }
+            else {
+                stack.push(comp);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()) {
+            sb.insert(0, "/" + stack.pop());
+        }
+        return sb.length() == 0 ? "/" : sb.toString();
+    }
+}
