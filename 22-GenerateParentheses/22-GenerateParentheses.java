a2c696e18d421231b2f4004b85e287ea23caa1f3
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-07-27 10:15:07 +0530

    Create 22-GenerateParentheses

---
+++ 22-GenerateParentheses/22-GenerateParentheses.java
@@ -0,0 +1,20 @@
+// Last updated: 27/07/2026, 10:15:08
+class Solution {
+    public void generate(int n,int l,int r,String s, List<String> ans){
+        if(r == n) {
+            ans.add(s);
+            return;
+        }
+        if(l < n) {
+            generate(n,l+1,r,s+"(",ans);
+        }
+        if(r < l) {
+            generate(n,l,r+1,s+")",ans);
+        }
+    }
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        generate(n, 0, 0, "", ans);
+        return ans;
+    }
+}
