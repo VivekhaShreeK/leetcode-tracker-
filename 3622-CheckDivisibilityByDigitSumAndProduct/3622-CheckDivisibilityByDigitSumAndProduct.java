de7dc75c427994edd14d50f09eab2a4c9757264f
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-09-23 23:04:20 +0530

    Create 3622-CheckDivisibilityByDigitSumAndProduct

---
+++ 3622-CheckDivisibilityByDigitSumAndProduct/3622-CheckDivisibilityByDigitSumAndProduct.java
@@ -0,0 +1,15 @@
+// Last updated: 23/09/2026, 23:04:19
+class Solution {
+    public boolean checkDivisibility(int n) {
+        int digitSum = 0;
+        int digitProduct = 1;
+        int original = n;
+        while (n > 0) {
+            int digit = n % 10;
+            n /= 10;
+            digitSum += digit;
+            digitProduct *= digit;
+        }
+        return original % (digitSum + digitProduct) == 0;
+    }
+}
