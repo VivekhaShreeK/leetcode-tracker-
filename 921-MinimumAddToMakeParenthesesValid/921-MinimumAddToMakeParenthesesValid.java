4131050bad0a19eb108137cc708bf7b5c1c1e8a8
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-09-23 23:16:03 +0530

    Create 921-MinimumAddToMakeParenthesesValid

---
+++ 921-MinimumAddToMakeParenthesesValid/921-MinimumAddToMakeParenthesesValid.java
@@ -0,0 +1,19 @@
+// Last updated: 23/09/2026, 23:16:02
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int openCount = 0; 
+        int closeCount = 0; 
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                openCount++; 
+            } else {
+                if (openCount > 0) {
+                    openCount--; 
+                } else {
+                    closeCount++; 
+                }
+            }
+        }
+        return openCount + closeCount;
+    }
+}
