ccba706de1de8625b2bc4617d35db7544e35e299
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-09-23 23:12:20 +0530

    Create 1689-PartitioningIntoMinimumNumberOfDeciBinaryNumbers

---
+++ 1689-PartitioningIntoMinimumNumberOfDeciBinaryNumbers/1689-PartitioningIntoMinimumNumberOfDeciBinaryNumbers.java
@@ -0,0 +1,10 @@
+// Last updated: 23/09/2026, 23:12:20
+class Solution {
+    public int minPartitions(String n) {
+        int ans = 0;
+        for (int i = 0; i < n.length(); ++i) {
+            ans = Math.max(ans, n.charAt(i) - '0');
+        }
+        return ans;
+    }
+}
