497b2d440a6044743f08748968a5f529eee3a551
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-09-23 23:05:02 +0530

    Create 3513-NumberOfUniqueXorTripletsI

---
+++ 3513-NumberOfUniqueXorTripletsI/3513-NumberOfUniqueXorTripletsI.java
@@ -0,0 +1,14 @@
+// Last updated: 23/09/2026, 23:05:01
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        int n = nums.length;
+        if (n <= 2) {
+            return n;
+        }
+        int ans = 1;
+        while (ans <= n) {
+            ans <<= 1;
+        }
+        return ans;
+    }
+}
