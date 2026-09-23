02cb73b6fd80ee26e7ab05361f1bb41d81241c6d
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-09-23 23:19:13 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.java
@@ -0,0 +1,14 @@
+// Last updated: 23/09/2026, 23:19:12
+class Solution {
+    public boolean canJump(int[] nums) {
+        int goal = nums.length - 1;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (i + nums[i] >= goal) {
+                goal = i;
+            }
+        }
+
+        return goal == 0;        
+    }
+}
