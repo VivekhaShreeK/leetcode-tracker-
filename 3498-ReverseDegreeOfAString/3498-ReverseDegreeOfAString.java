5865e9d8de89753686eff4b639c872d63f91ba78
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-09-23 23:05:15 +0530

    Create 3498-ReverseDegreeOfAString

---
+++ 3498-ReverseDegreeOfAString/3498-ReverseDegreeOfAString.java
@@ -0,0 +1,11 @@
+// Last updated: 23/09/2026, 23:05:14
+class Solution {
+
+    public int reverseDegree(String s) {
+        int ans = 0;
+        for (int i = 1; i <= s.length(); i++) {
+            ans += (26 - (s.charAt(i - 1) - 'a')) * i;
+        }
+        return ans;
+    }
+}
