296b4566ced409e5a58cfa88185603c42729315e
Vivekha Shree Karthikeyan <vivekhashree.k@gmail.com>
2026-09-23 23:03:08 +0530

    Create 3702-LongestSubsequenceWithNonZeroBitwiseXor

---
+++ 3702-LongestSubsequenceWithNonZeroBitwiseXor/3702-LongestSubsequenceWithNonZeroBitwiseXor.java
@@ -0,0 +1,18 @@
+// Last updated: 23/09/2026, 23:03:08
+class Solution {
+    public int longestSubsequence(int[] nums) {
+        int n = nums.length;
+        int totalXor = 0;
+        boolean allZero = true;
+        for (int x : nums) {
+            totalXor ^= x;
+            if (x > 0) {
+                allZero = false;
+            }
+        }
+        if (totalXor > 0) {
+            return n;
+        }
+        return allZero ? 0 : n - 1;
+    }
+}
